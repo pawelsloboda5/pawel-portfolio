1da033ee5f12aa558d7ee1d25524d7d651e056c2
pawelsloboda5 <pawelsloboda5@gmail.com>
2025-10-06 02:23:11 -0400

    full working with links

--- src/data/links.ts
+++ src/data/links.ts
@@ -14,8 +14,19 @@ export const socialLinks = {
 export const companyLinks = {
   sie2: 'https://www.sie2.com/copilot',
   adobe: 'https://www.adobe.com',
-  ndu: 'https://www.ndu.edu'
+  ndu: 'https://www.ndu.edu',
+  HealthCareNavigator: 'https://healthcare-cost-navigator.vercel.app/',
+  DigitizeMD: 'https://www.digitizemd.com/'
+} as const;
+
+export const projectGithubLinks = {
+  HealthCareNavigator: 'https://github.com/pawelsloboda5/Healthcare-Cost-Navigator',
+  NPILookupMCP: 'https://github.com/pawelsloboda5/NPI_Lookup_MCP_Server',
+  RustHax: 'https://github.com/pawelsloboda5/rust-YOLOv8-PlayerDetection',
+  MLTradingStrategy: 'https://github.com/pawelsloboda5/ML-Trading-Strategy',
+  CalworksAnalysis: 'https://github.com/pawelsloboda5/calworks-analysis',
 } as const;
 
 export type SocialLinks = typeof socialLinks;
 export type CompanyLinks = typeof companyLinks;
+export type ProjectGithubLinks = typeof projectGithubLinks;
